7b20156b9b2251c2e5ca4b2c18d8e7e13ecdd193
KARINA LIZETH ALVAREZ ANDINO <klalvarez@uth.hn>
2025-07-25 22:34:02 -0600

    nuevo proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoCalculadora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
